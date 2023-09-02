c3bae31a8e53bbcccd1ad33793a4b6645695c86b
xiayefeng <xiayu_12@yeah.net>
2023-09-02 11:20:21 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,17 @@
 /**
  * 二维数组合并单元格（仅针对数组对象本身）
- * @param data 接口返回的数组对象
- * @param field 指定合并选项为数组对象的某个属性值
- * @param colArr 需要合并列的下标
- * @returns mergeCells单元格合并规则
+ * @param data {array} 接口返回的数组对象
+ * @param field {array|string} 指定合并选项为数组对象的某个属性值
+ * @param colArr {number[]} 需要合并列的下标
+ * @returns mergeCells {{row: number, col: number, colspan: number, colspan: number}[] }单元格合并规则
  */
 export function parseMergeCell (data, field, colArr) {
   let mergeCells = []
   let repeatVal = ''
   let rowspan = 1
   for (let i = 0; i < data.length; i++) {
-    let { [field]: fieldVal } = data[i]
+    // let { [field]: fieldVal } = data[i]
+    let fieldVal = getCellsName(data[i], field)
     if (repeatVal === fieldVal) {
       rowspan++
       if (i === data.length - 1) {
@@ -33,4 +34,14 @@ export function parseMergeCell (data, field, colArr) {
 
   }
   return mergeCells
+}
+
+function getCellsName (row, fields) {
+  if (typeof fields === 'string') {
+    return row[fields] ?? ''
+  } else if (Array.isArray(fields)) {
+    return fields.map(item => row[item] ?? '').join()
+  } else {
+    return ''
+  }
 }
